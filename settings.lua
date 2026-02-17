fb8d273eb6f8acee593e4cc965c017434a5e1e8d
denzells <denzellbest@gmail.com>
2026-02-17 13:58:11 +0100

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -898,10 +898,9 @@ function Settings.build(page, r)
             Size = UDim2.new(0, 18, 0, 18),
             Position = UDim2.new(1, -26, 0, 7),
             BackgroundTransparency = 1,
-            ImageColor3 = C.RED,
+            ImageColor3 = Color3.fromRGB(70, 70, 70),
             ZIndex = 6,
         }, customPanel.Parent)
-        table.insert(accentEls, { el = cpIcon, prop = "ImageColor3" })
         CreateAccentPicker(customPanel)
         CreateFontPicker(customPanel)
 
@@ -913,10 +912,9 @@ function Settings.build(page, r)
             Size = UDim2.new(0, 18, 0, 18),
             Position = UDim2.new(1, -26, 0, 7),
             BackgroundTransparency = 1,
-            ImageColor3 = C.RED,
+            ImageColor3 = Color3.fromRGB(70, 70, 70),
             ZIndex = 6,
         }, keybindPanel.Parent)
-        table.insert(accentEls, { el = kbIcon, prop = "ImageColor3" })
         CreateKeybinds(keybindPanel)
 
         -- Panel Info Sesion
